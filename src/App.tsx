73144eada16373b98d072f38c5d93dc45a6f0ff5
vaibhavAccionLabs <vaibhav.katariya@accionlabs.com>
2021-11-09 15:04:47 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+type Props = {
+  test: string;
+};
+
+const App = ({ test }: Props) => (
+  <div className='App'>
+    <header className='App-header'>
+      <img src={logo} className='App-logo' alt='logo' />
+      <p>
+        Edit <code>src/App.tsx</code>
+        and save to reload.
+        {test}
+      </p>
+
+      <a
+        className='App-link'
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'>
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
+
+/**
+ * Follow :
+ * https://create-react-app.dev/
+ * https://karthickragavendran.medium.com/protect-react-codebase-with-eslint-prettier-typescript-lint-staged-and-husky-9dd549bdb1c
+ *
+ * For Non-Ts
+ * https://dev.to/ankitt8/setting-up-eslint-airbnb-configuration-prettier-and-husky-pre-commit-hooks-in-cra-5dbo
+ */
